d011c1bc9792660f4fcd22cdd0569fafe5bbc3f0
javf1016 <ja.vera@uniandes.edu.co>
2023-02-07 12:03:19 -0500

    Initial commit

---
+++ src/main/java/com/superheroe/api/ApiSuperHeroeApplication.java
@@ -0,0 +1,13 @@
+package com.superheroe.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSuperHeroeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSuperHeroeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superheroe/api/ApiSuperHeroeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superheroe.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSuperHeroeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
